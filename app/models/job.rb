161947f6b8541a80353fd908375a10c17d49954b
Tony Gaeta <tony.gaeta@gmail.com>
2015-02-22 21:47:03 -0500

    added validations to job applications

--- app/models/job.rb
+++ app/models/job.rb
@@ -14,7 +14,7 @@
 class Job < ActiveRecord::Base
   belongs_to :company
   has_many :job_applications
-  validates :company, presence: true
+  validates :company_id, presence: true
   validates :position, presence: true
   validates :description, presence: true
   validates :description, length: { minimum: 500 }
--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: job_applications
+#
+#  id           :integer          not null, primary key
+#  name         :string
+#  resume       :text
+#  cover_letter :text
+#  job_id       :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class JobApplication < ActiveRecord::Base
   belongs_to :job
+  validates :name, presence: true
+  validates :resume, presence: true
+  validates :cover_letter, presence: true
+  validates :job_id, presence: true
 end
