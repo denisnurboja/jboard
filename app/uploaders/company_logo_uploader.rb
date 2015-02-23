796b4ba72f6635059f2b90300b38c34942f495d9
Tony Gaeta <tony.gaeta@gmail.com>
2015-02-23 18:34:08 -0500

    added thumbnail to index view, sorted view to have thumb display at top

--- app/uploaders/company_logo_uploader.rb
+++ app/uploaders/company_logo_uploader.rb
@@ -4,14 +4,11 @@ class CompanyLogoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -34,15 +31,15 @@ class CompanyLogoUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_limit => [25, 25]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
