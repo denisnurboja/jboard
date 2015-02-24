87635fbf4d95a80767b23db0f7a2d2688d1c2f16
Tony Gaeta <tony.gaeta@gmail.com>
2015-02-24 01:20:02 -0500

    made seeds.rb more readable

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,17 @@ Company.destroy_all
 unless Company.count > 0
   20.times do
     company = Company.create!(remote_company_logo_url: Faker::Company.logo,
-                              name: Faker::Company.name + ' ' +
-                              Faker::Company.suffix)
+                              name:
+                              Faker::Company.name + ' ' + Faker::Company.suffix)
     2.times do
-      job = company.jobs.create!(position: Faker::Name.title, location:
-      Faker::Address.country, description: Faker::Lorem.paragraph(10))
+      job = company.jobs.create!(position: Faker::Name.title,
+                                 location: Faker::Address.country,
+                                 description: Faker::Lorem.paragraph(10))
       2.times do
-        jobapp = job.job_applications.create(name: Faker::Name.name, resume:
-        Faker::Lorem.sentence(3), cover_letter: Faker::Lorem.sentence(3), remote_image_url: Faker::Avatar.image)
+        j = job.job_applications.create(name: Faker::Name.name,
+                                        resume: Faker::Lorem.sentence(3),
+                                        cover_letter: Faker::Lorem.sentence(3),
+                                        remote_image_url: Faker::Avatar.image)
       end
     end
   end
